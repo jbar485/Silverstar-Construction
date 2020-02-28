4c56fa5988e3d76598778980afc463f24cfacd1b
Judah Barton <judah.barton@yahoo.com>
2020-02-28 14:55:21 -0800

    add Routing to both my Home Page and Error page as well as basic code to make it work

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Error404 from './Error404';
+import Home from './Home';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
-      
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route component={Error404}/>
+      </Switch>
+
     </div>
   );
 }
