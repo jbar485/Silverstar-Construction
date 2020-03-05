cb48f3b36c96e7a324b6273cd1c79908ae57e43e
Judah Barton <judah.barton@yahoo.com>
2020-03-05 09:41:40 -0800

    add some more to my basic setup for my website

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import './App.css';
-import Error404 from './Error404';
-import Home from './Home';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Error404 from "./Error404";
+import Landing from "./Landing";
+import HomeList from "./HomeList";
+import About from "./About";
+import Contact from "./Contact";
+import MasterHomeList from "./MasterHomeList";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="">
+      <Header />
       <Switch>
-        <Route exact path='/' component={Landing}/>
-        <Route component={Error404}/>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/homes" component={HomeList} />
+        <Route component={Error404} />
       </Switch>
-
+      <Footer />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 
 function Home() {
   return (
-    <h1>Home Page</h1>
+    <h1>Page</h1>
   );
 }
 
