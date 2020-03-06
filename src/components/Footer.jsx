05ad9481ff4c499608f61e69a6fd369d23e9a214
Judah Barton <judah.barton@yahoo.com>
2020-03-06 12:01:04 -0800

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-content">
-        <div className="footer-section about"></div>
-        <div className="footer-section links"></div>
-        <div className="footer-section contact-form"></div>
+        <div className="footer-section-logo"></div>
+        <div className="footer-section-pages"></div>
+        <div className="footer-section-links"></div>
       </div>
       <div className="footer-bottom">
         &copy; 2020 silverstarconstruction.com | Designed by Judah Barton
