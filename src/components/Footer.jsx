fae3988b4f2a544f9561b6697338ce71b7083f7b
Judah Barton <judah.barton@yahoo.com>
2020-03-11 16:58:25 -0700

    trying to figure out how to make my image gallery more inclusive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ function Footer() {
         </div>
         <div className="footer-links">
           <li>
-            <a href="https://www.facebook.com/silverstar.homes.1" target="_blank">
+            <a href="https://www.facebook.com/Silverstar-Construction-103907397898036/" target="_blank">
               <img
                 src={facebook}
                 alt="Facebook Link"
